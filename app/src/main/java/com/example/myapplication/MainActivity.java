0e79cfa8c1cac37c43b0c68c3922148434810935
aperpen <aperpen.an@gmail.com>
2022-09-21 02:42:15 +0300

    Revert color change functionality

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,21 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button changeTextBtn = (Button) findViewById(R.id.changeText);
-        changeTextBtn.setOnClickListener(v -> {
+        Button button = (Button) findViewById(R.id.changeText);
+        button.setOnClickListener(v -> {
             TextView textLabel = findViewById(R.id.textLabel);
-            int randomNumber = (int) (Math.random() * 100 + 1); // Random number between 1 and 100
+            int randomNumber = (int)(Math.random() * 100 + 1); // Random number between 1 and 100
 
             textLabel.setText(String.format(getString(R.string.changed_text), randomNumber));
-        });
-
-        Button changeColorBtn = (Button) findViewById(R.id.changeColor);
-        changeColorBtn.setOnClickListener(v -> {
-            TextView textLabel = findViewById(R.id.textLabel);
-            // Generate random color: https://stackoverflow.com/a/55334738
-            int color = ((int) (Math.random() * 16777215)) | (0xFF << 24);
-
-            textLabel.setTextColor(color);
         });
     }
 }
